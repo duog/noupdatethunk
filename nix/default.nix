174bb541e7a8692a1abd7d74f9a172381048523d
Douglas Wilson <douglas.wilson@gmail.com>
2021-07-08 15:28:30 +0100

    head.hackage

--- nix/default.nix
+++ nix/default.nix
@@ -4,14 +4,8 @@ with builtins;
 , overlays ? []
 , system ? currentSystem
 }: let
-  head-hackage-overlay = self: super:
-    import ./head.hackage {
-      haskellOverrides = hself: hsuper: {
-        hscolour = with self.haskell.lib; overrideSrc hsuper.hscolour {
-          src = ../hscolour-1.24.4;
-        };
-      };
-    } self super;
+  # Note this is my branch
+  head-hackage-overlay = import sources.headhackage {};
 in import sources.nixpkgs {
   inherit config system;
   overlays = overlays ++ [head-hackage-overlay];
